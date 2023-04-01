af72ccf278f61c6cb96097a97a3fbce6f3e44766
MarcusWebDev <marcusbrookswebdev@gmail.com>
2023-04-01 15:12:54 -0400

    first commit

---
+++ src/Components/AmiiboCard/AmiiboCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './AmiiboCard.css';
+
+const AmiiboCard = ({colorArray, amiibo}) => {
+    return (
+        <div className="amiiboCardContainer">
+            <div className="amiiboCardTextContainer">
+
+            </div>
+        </div>
+    );
+}
+
+export default AmiiboCard;
