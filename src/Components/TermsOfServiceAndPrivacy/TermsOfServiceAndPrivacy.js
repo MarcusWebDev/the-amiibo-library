b62e251ffa87da8d1edbe9ec8e3c86a57f82f410
MarcusWebDev <marcusbrookswebdev@gmail.com>
2023-04-21 22:21:32 -0400

    set up gh-pages

--- src/Components/TermsOfServiceAndPrivacy/TermsOfServiceAndPrivacy.js
+++ src/Components/TermsOfServiceAndPrivacy/TermsOfServiceAndPrivacy.js
@@ -6,7 +6,7 @@ const TermsOfServiceAndPrivacy = ({}) => {
         <div className="TOSAPContainer">
             <h1 className="TOSAPHeader">Terms of Service / Privacy Policy</h1>
             <p>By using this site you acknowlege that this site is an unofficial, fan-made creation and has no affiliation with Nintendo or any other company whose intellectual property or products are depicted here.</p>
-            <p>The only personal information that The Amiibo Library collects and stores is your email address. This is to allow you to use google sign in and manage your account on The Amiibo Library.</p>
+            <p>The only personal information that The Amiibo Library collects and stores is your email address. This is to allow you to use google sign in and manage your collection on The Amiibo Library.</p>
         </div>
     );
 }
